ddad976d7efd334cb00adc2a3e2bce18bb0ea2ce
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 15:35:22 -0600

    finished section

---
+++ LearningUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningUsing
+{
+    class Program
+    {
+        class MyResource : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("disposing our resource");
+            }
+        }
+        static void Main(string[] args)
+        {
+            using(var resource = new MyResource())
+            {
+                Console.WriteLine("Using my resource");
+            }
+            Console.WriteLine("Finished");
+        }
+    }
+}
